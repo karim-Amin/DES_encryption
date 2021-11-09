5a6b6d040e5801c4dc0aa9c02bdf7a68ab085dea
Kareeem <kareem.asu11@gmail.com>
2021-11-09 12:47:03 +0200

    Add project files.

---
+++ DES_encryption/1701009.cpp
@@ -0,0 +1,23 @@
+/*
+* File name : 1701009.cpp
+* Description : this file will include the impelementation for DES security algorithm there will be two options to select to encrypt 
+* any message and the key will be required to compelete the process ,the second one is to decrype the cipher text using specific key.
+* Autho : Karim Mohamed Amin
+* Date : 9/11/2021
+*/
+
+#include <iostream>
+using namespace std;
+/**********************************************************************
+*                               Function Prototypes                   *
+***********************************************************************/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
+/**********************************************************************
+*                               Function Definitions                  *
+***********************************************************************/
+
