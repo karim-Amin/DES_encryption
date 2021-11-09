a84a6d73ad7ff06a22d65e6cd79d3ade6c337e37
Kareeem <kareem.asu11@gmail.com>
2021-11-09 21:13:23 +0200

    substitution permutation

--- DES_encryption/1701009.cpp
+++ DES_encryption/1701009.cpp
@@ -7,7 +7,7 @@
 */
 
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <map>
 using namespace std;
 /**********************************************************************
@@ -15,6 +15,7 @@ using namespace std;
 ***********************************************************************/
 #define LEFT_KEY_PART_SIZE 28
 #define XOR_GATE_OUTPUT_SIZE 48
+#define DES_NUM_OF_STAGES 16
 /**********************************************************************
 *                               Function Prototypes                   *
 ***********************************************************************/
@@ -24,6 +25,8 @@ string getPermuted(string input_text, int permute_table[], int table_size);
 string leftCircularShift(string input_text,int num_shifts);
 string xorGate(string input1, string input2);
 string encrypt(string plain_text,string key);
+string substitutionPermuted(string input_text,int sub_table[8][4][16],int num_tables,int num_rows,int num_colmuns);
+int binaryToDecimal(int n);
 int main()
 {
  
@@ -105,7 +108,8 @@ string convertHexToBin(string hex) {
     return binary_format;
 }
 /*
-* Description : this function will apply the permutaion as required ,and it will take the input message in binary format
+* Description : this function will apply the permutaion as required 
+*,and it will take the input message in binary format
 * finally it will return the text after permutaion in binary format
 */
 string getPermuted(string input_text, int permute_table[], int size) {
@@ -159,7 +163,8 @@ string xorGate(string input1, string input2) {
     return result;
 }
 /*
-* Description : this function do all premutations in DES ,also it generates all subkeys,and impelement all the sixteen rounds
+* Description : this function do all premutations in DES 
+* also it generates all subkeys,and impelement all the sixteen rounds
 * finally it will return the cipher text
 */
 string encrypt(string plain_text,string key) {
@@ -195,14 +200,14 @@ string encrypt(string plain_text,string key) {
     string left_half = compresed_key.substr(0,28);
     string right_half = compresed_key.substr(28, 28);
     /*apply left circular shift using the table above each key will be 56 bits*/
-    string shifted_keys[16];
-    for (int i = 0; i < 16; i++) {
+    string shifted_keys[DES_NUM_OF_STAGES];
+    for (int i = 0; i < DES_NUM_OF_STAGES; i++) {
         /*after applying the left circular shift ,i merged the two halves still the key size is 56 bits*/
         shifted_keys[i] = leftCircularShift(left_half, shift_table[i]) + leftCircularShift(right_half, shift_table[i]);
     }
     /*apply premutation choice 2 this key will be 48 bits*/
-    string subkeys[16];
-    for (int i = 0; i < 16; i++) {
+    string subkeys[DES_NUM_OF_STAGES];
+    for (int i = 0; i < DES_NUM_OF_STAGES; i++) {
         subkeys[i] = getPermuted(shifted_keys[i], premuted_choice2_table,48);
     }
     /*now we have all subkeys we can use them in encryption*/
@@ -215,19 +220,138 @@ string encrypt(string plain_text,string key) {
                                           59, 51, 43, 35, 27, 19, 11, 3,
                                           61, 53, 45, 37, 29, 21, 13, 5,
                                           63, 55, 47, 39, 31, 23, 15, 7 };
-    string permutated_plain_text = getPermuted(plain_text, initial_permutation_table, 64);
-    /*now to follow the DES standard we have to split plain text into two halves*/
-    string plain_left_half = permutated_plain_text.substr(0, 32);
-    string plain_right_half = permutated_plain_text.substr(32, 32);
     /* expension permutation table*/
-    /*this table will expand our plain text from 32 bits into 48 bits*/
+  /*this table will expand our plain text from 32 bits into 48 bits*/
     int expansion_permutation_table[48] = { 32, 1, 2, 3, 4, 5, 4, 5,
                                             6, 7, 8, 9, 8, 9, 10, 11,
                                            12, 13, 12, 13, 14, 15, 16, 17,
                                            16, 17, 18, 19, 20, 21, 20, 21,
                                            22, 23, 24, 25, 24, 25, 26, 27,
                                            28, 29, 28, 29, 30, 31, 32, 1 };
+    /*substitution box table each six bits will be compersed into 4 bits based on table */
+    /*we have 8 tables with 4 rows and 16 columns*/
+    int substitution_table[8][4][16] = { { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+                          0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+                          4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+                          15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 },
 
+                        { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+                          3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+                          0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+                          13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 },
 
+                        { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+                          13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+                          13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+                          1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 },
+
+                        { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+                          13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+                          10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+                          3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 },
+
+                        { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+                          14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+                          4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+                          11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 },
+
+                        { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+                          10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+                          9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+                          4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 },
+
+                        { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+                          13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+                          1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+                          6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 },
+
+                        { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+                          1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+                          7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+                          2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } };
+    // permutaion table 
+    int permutaion_table[32] = {16, 7, 20, 21,
+                                29, 12, 28, 17,
+                                1, 15, 23, 26,
+                                5, 18, 31, 10,
+                                2, 8, 24, 14,
+                                32, 27, 3, 9,
+                                19, 13, 30, 6,
+                                22, 11, 4, 25 };
+    string permutated_plain_text = getPermuted(plain_text, initial_permutation_table, 64);
+    /*now to follow the DES standard we have to split plain text into two halves*/
+    string plain_left_half = permutated_plain_text.substr(0, 32);
+    string plain_right_half = permutated_plain_text.substr(32, 32);
+    /*holds expanded plain text left half*/
+    string expanded_right_plain;
+    /*holds the result from the xor operation*/
+    string xor_result;
+    /*this loop for 16 rounds*/
+    for (int i = 0; i < DES_NUM_OF_STAGES; i++) {
+        /*expanding the plain text left half which will be 48 bits */
+        expanded_right_plain = getPermuted(plain_right_half, expansion_permutation_table, 48);
+        xor_result = xorGate(expanded_right_plain, subkeys[i]);
+        /*the size of this plain text will be 32 bits again */
+        permutated_plain_text = substitutionPermuted(xor_result, substitution_table, 8, 4, 16);
+        while (1);
+    }
     return "";
+}
+/*
+ * Description : this function receives the input_text which is 48 bits and devide them into 8 groups each group is six bits
+ * then substitute in the tables ,and permute each group to 4 bits only so we will get 32 bits plain text
+ */
+string substitutionPermuted(string input_text, int sub_table[8][4][16], int num_tables, int num_rows, int num_colmuns) {
+    /*this will hold our text after permutation*/
+    string text_after_permutation = "";
+    /*this holds the index from the tabel*/
+    int new_index;
+    /*loop counter*/
+    int i;
+    /*counter will split the text into 8 groups consist of 6 bits*/
+    int split_count;
+    /*holds the six bits in 48 bits*/
+    string six_bits = "";
+    string row_num ;
+    string column_num;
+    /*loop across all the groups*/
+    for (i = 0; i < num_tables; i++) {
+        /*this takes six bits each iteration*/
+        split_count = i * 6;
+        six_bits = input_text.substr(split_count,6);
+        /*the row number will be the first and last bits*/
+        row_num = six_bits.substr(0,1)+ six_bits.substr(5,1);
+        /*the column number will be the four middel bits*/
+        column_num = six_bits.substr(1,4);
+        /*convert the string and the binary into decimal value to access the sub table*/
+        int int_row_num = binaryToDecimal(stoi(row_num));
+        int int_column_num = binaryToDecimal(stoi(column_num));
+        /*accessing the sub table and get the new index*/
+        new_index = sub_table[i][int_row_num][int_column_num] - 1;
+        text_after_permutation += input_text[new_index];
+    }
+    return text_after_permutation;
+}
+/* 
+ * Description : to convert the binary code to decimal value   
+ */
+int binaryToDecimal(int n)
+{
+    int num = n;
+    int dec_value = 0;
+
+    // Initializing base value to 1, i.e 2^0
+    int base = 1;
+
+    int temp = num;
+    while (temp) {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+
+        dec_value += last_digit * base;
+
+        base = base * 2;
+    }
+
+    return dec_value;
 }
